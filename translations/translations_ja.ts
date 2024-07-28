66c65a3a394f9ccf7a0a539e553641d19dd1d79f
Azure <azurexsx@gmail.com>
2024-07-28 18:54:18 +0200

    Added taanslation files

---
+++ translations/translations_ja.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>SignIn</name>
+    <message>
+        <location filename="../content/SignIn.qml" line="65"/>
+        <source>Sign In</source>
+        <translation>ログイン</translation>
+    </message>
+    <message>
+        <location filename="../content/SignIn.qml" line="76"/>
+        <source>Username</source>
+        <translation>ユーザー名</translation>
+    </message>
+    <message>
+        <location filename="../content/SignIn.qml" line="137"/>
+        <source>Password</source>
+        <translation>パスワード</translation>
+    </message>
+</context>
+<context>
+    <name>SignUp</name>
+    <message>
+        <location filename="../content/SignUp.qml" line="21"/>
+        <source>Sign In</source>
+        <translation>ログイン</translation>
+    </message>
+    <message>
+        <location filename="../content/SignUp.qml" line="70"/>
+        <source>Sign Up</source>
+        <translation>登録</translation>
+    </message>
+    <message>
+        <location filename="../content/SignUp.qml" line="81"/>
+        <source>Username</source>
+        <translation>ユーザー名</translation>
+    </message>
+    <message>
+        <location filename="../content/SignUp.qml" line="116"/>
+        <source>Username must be at least 3 characters long and contain only letters and numbers.</source>
+        <translation>ユーザーネームは3文字以上で、半角英数字のみです。</translation>
+    </message>
+    <message>
+        <location filename="../content/SignUp.qml" line="170"/>
+        <source>Email</source>
+        <translation>電子メール</translation>
+    </message>
+    <message>
+        <location filename="../content/SignUp.qml" line="205"/>
+        <source>Please enter a valid email adress.</source>
+        <translation>有効なメールアドレスを入力してください。</translation>
+    </message>
+    <message>
+        <location filename="../content/SignUp.qml" line="260"/>
+        <source>Password</source>
+        <translation>パスワード</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/translations_uk.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>SignIn</name>
+    <message>
+        <location filename="../content/SignIn.qml" line="65"/>
+        <source>Sign In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../content/SignIn.qml" line="76"/>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../content/SignIn.qml" line="137"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SignUp</name>
+    <message>
+        <location filename="../content/SignUp.qml" line="21"/>
+        <source>Sign In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../content/SignUp.qml" line="70"/>
+        <source>Sign Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../content/SignUp.qml" line="81"/>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../content/SignUp.qml" line="116"/>
+        <source>Username must be at least 3 characters long and contain only letters and numbers.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../content/SignUp.qml" line="170"/>
+        <source>Email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../content/SignUp.qml" line="205"/>
+        <source>Please enter a valid email adress.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../content/SignUp.qml" line="260"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
