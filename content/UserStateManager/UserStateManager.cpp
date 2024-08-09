2af83254148050bb959d47e441feffff05bb57a5
Azure <azurexsx@gmail.com>
2024-08-09 12:37:04 +0200

    UserStateManager update, implemented avatar change

--- content/UserStateManager/UserStateManager.cpp
+++ content/UserStateManager/UserStateManager.cpp
@@ -1,6 +1,41 @@
 #include "UserStateManager.h"
+#include "qglobal.h"
+#include "qobjectdefs.h"
+#include "qurl.h"
 
 UserStateManager::UserStateManager(QObject *parent) : QObject(parent) {}
 
 UserStateManager::~UserStateManager() {}
 
+void UserStateManager::selectAvatar(const QUrl &url) {
+    qDebug() << "Url: " << url;
+    setAvatar(url);
+}
+
+void UserStateManager::setAvatar(const QUrl &avatar) {
+    m_avatar = avatar;
+    emit avatarChanged();
+}
+
+QUrl UserStateManager::avatar() const { return m_avatar; }
+
+void UserStateManager::setUserDisplayName(const QString &userDisplayName) {
+    m_userDisplayName = userDisplayName;
+    emit userDisplayNameChanged();
+}
+
+QString UserStateManager::userDisplayName() const { return m_userDisplayName; }
+
+void UserStateManager::setUsername(const QString &username) {
+    m_username = username;
+    emit usernameChanged();
+}
+
+QString UserStateManager::username() const { return m_username; }
+
+void UserStateManager::setEmail(const QString &email) {
+    m_email = email;
+    emit emailChanged();
+}
+
+QString UserStateManager::email() const { return m_email; }
--- content/UserStateManager/UserStateManager.h
+++ content/UserStateManager/UserStateManager.h
@@ -1,35 +1,54 @@
 #ifndef USER_STATE_MANAGER_H
 #define USER_STATE_MANAGER_H
 
+#include "qhashfunctions.h"
+#include "qobjectdefs.h"
+#include "qurl.h"
 #include <QByteArray>
 #include <QDebug>
 #include <QImage>
 #include <QObject>
-#include <QImage>
 #include <QString>
 
 class UserStateManager : public QObject {
     Q_OBJECT
-
-    Q_PROPERTY(QImage avatar READ avatar NOTIFY avatarChanged);
-    Q_PROPERTY(QString userDisplayName READ userDisplayName NOTIFY userDisplayNameChanged);
-    Q_PROPERTY(QString username READ username NOTIFY usernameChanged);
-    Q_PROPERTY(QString email READ email NOTIFY emailChanged);
-    Q_PROPERTY(QString password READ password NOTIFY passwordChanged);
+    Q_PROPERTY(QUrl avatar READ avatar WRITE setAvatar NOTIFY avatarChanged)
+    Q_PROPERTY(QString userDisplayName READ userDisplayName WRITE
+                   setUserDisplayName NOTIFY userDisplayNameChanged)
+    Q_PROPERTY(
+        QString username READ username WRITE setUsername NOTIFY usernameChanged)
+    Q_PROPERTY(QString email READ email WRITE setEmail NOTIFY emailChanged)
 
   public:
     explicit UserStateManager(QObject *parent = nullptr);
-
     ~UserStateManager();
 
-    void setUserAvatar(const QByteArray &avatar);
-    QByteArray getUserAvatar() const;
+    Q_INVOKABLE void selectAvatar(const QUrl &);
+
+    QUrl avatar() const;
+
+    QString userDisplayName() const;
+
+    QString username() const;
+
+    QString email() const;
+
   signals:
     void avatarChanged();
+    void userDisplayNameChanged();
+    void usernameChanged();
+    void emailChanged();
 
   private:
-    QByteArray m_userAvatar;
-    
+    void setAvatar(const QUrl &avatar);
+    void setUserDisplayName(const QString &name);
+    void setUsername(const QString &username);
+    void setEmail(const QString &email);
+
+    QUrl m_avatar;
+    QString m_userDisplayName;
+    QString m_username;
+    QString m_email;
 };
 
 #endif // USER_STATE_MANAGER_H
